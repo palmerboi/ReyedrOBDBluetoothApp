d019e18d8d06c0c48ca3e0ed55ad4a51ec08d281
El Jefe <reuben.j.palmer@gmail.com>
2018-09-15 21:24:59 +1200

    Initial commit/Created Project

---
+++ app/src/main/java/com/example/reube/reyedrobdbluetoothapp/ConnectorSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.reube.reyedrobdbluetoothapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectorSelectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connector_selection);
+    }
+}
